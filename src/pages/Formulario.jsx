c534ae987042babb7147fe2a9f997c9401f4e54a
SeriousLess <diegomoscoso2019@gmail.com>
2025-08-11 12:46:31 -0500

    first commit

--- src/pages/Formulario.jsx
+++ src/pages/Formulario.jsx
@@ -36,7 +36,7 @@ export default function Formulario() {
     setResultado(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/prediccion/", {
+      const response = await fetch("https://diabetes-prediction-3-bakd.onrender.com/prediccion/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(
